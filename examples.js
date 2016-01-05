e20a31c5e86b2e3aa5576ef3b1d41768d369821e
Wessel Loth <wessel.loth@gmail.com>
2016-01-05 20:20:21 +0000

    Initial release

---
+++ examples.js
@@ -0,0 +1,6 @@
+var ToukuFM = require('./index');
+
+var inst = new ToukuFM();
+//inst.get_now_playing(console.log);
+//inst.get_schedule_days_ahead(10, console.log);
+inst.get_zones(console.log);
---
+++ index.js
@@ -0,0 +1,45 @@
+var request = require('request');
+
+var ToukuFM = function () {
+
+    var _exec = function (path, callback) {
+        if (typeof callback !== 'function')
+            callback = console.log;
+        
+        var url = 'http://www.toukufm.com/data/' + path;
+
+        request.get(url, function (err, req, body) {
+            if (err) {
+                // TODO: err is always undefined?
+                console.log("Error");
+                callback({status: req.status});
+            }
+            
+            callback(JSON.parse(body));
+        });
+    };
+
+    // Public methods
+    return {
+        get_team: function (callback) {
+            _exec('team', callback);
+        },
+        get_now_playing: function (callback) {
+            _exec('nowplaying', callback);
+        },
+        get_schedule: function (callback) {
+            _exec('schedule', callback);
+        },
+        get_schedule_days_ahead: function (days, callback) {
+            if (!(typeof days === 'number' && (days % 1) === 0))
+                throw new Error("ToukuFM.get_schedule_days_ahead(): Days must be an integer.");
+            var url = 'schedule/' + days;
+            _exec(url, callback);
+        },
+        get_zones: function (callback) {
+            _exec('zones', callback);
+        }
+    };
+};
+
+module.exports = ToukuFM;
